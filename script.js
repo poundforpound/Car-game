56477b99f63fec8159a04bd8b6e88f470ce7e6ae
PoundforPound <a.zenit2014@yandex.ru>
2024-05-01 18:51:31 +0300

    lesson 3.1_refactoring get coordinate

--- script.js
+++ script.js
@@ -9,18 +9,23 @@
   const tree1 = trees[0];
 
   animationId = requestAnimationFrame(startGame);
+  const coordinateTree1 = getCoord(tree1);
 
-  function getCoordY(element) {
+  function getCoord(element) {
     const matrix = window.getComputedStyle(element).transform;
     const arrayOfCoordinate = matrix.split(',');
-    const lastElement = arrayOfCoordinate[arrayOfCoordinate.length - 1];
-    const coordY = parseFloat(lastElement);
-    return coordY;
+    const y = arrayOfCoordinate[arrayOfCoordinate.length - 1];
+    const x = arrayOfCoordinate[arrayOfCoordinate.length - 2];
+    const coordY = parseFloat(y);
+    const coordX = parseFloat(x);
+    return { x: coordX, y: coordY };
   }
 
   function treesAnimation() {
-    const newCoordY = getCoordY(tree1) + speed;
-    tree1.style.transform = `translateY(${newCoordY}px)`;
+    const newCoordY = coordinateTree1.y + speed;
+    coordinateTree1.y = newCoordY;
+    tree1.style.transform = `translate(${coordinateTree1.x}px,${newCoordY}px)`;
+    console.log(coordinateTree1.x, coordinateTree1.y);
   }
 
   function startGame() {
@@ -28,6 +33,7 @@
 
     animationId = requestAnimationFrame(startGame);
   }
+
   button.addEventListener('click', () => {
     isPause = !isPause;
     if (isPause) {
